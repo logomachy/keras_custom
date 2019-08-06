bef40225f37c62bf05390233dde781b84fb03c09
logomachy <monsieurcogito@gmail.com>
2019-08-06 13:09:17 +0200

    write helper functions that convert header and main article to characters

--- tidytext.R
+++ tidytext.R
@@ -6,6 +6,7 @@ library(magrittr)
 library(rebus)
 library(keras)
 library(tensorflow)
+library(tm)
 setwd("bbc/")
 #-------------
 list.dirs() %>% stringr::str_remove_all("\\.") %>% stringr::str_remove_all("/") %>%
@@ -24,8 +25,47 @@ caret::createDataPartition(DATA$category, p = 0.8, list = F) -> in_train
 DATA %<>% mutate(in_train = ifelse(name %in% in_train, T, F))
 DATA %<>% mutate(text = map_chr(text, unlist )) 
 #--------------
+# remove digits
 num_pattern <- one_or_more(DGT)
+# remove stopwords
+DATA %>%
+  mutate(text = map_chr(text, str_remove_all, pattern = num_pattern ) ) %>%
+  mutate(header = map(text, str_split, pattern = "\n\n") %>%
+           map(~.x[[1]][[1]] )) %>%
+  mutate(header = as.character(header) ) %>%
+  mutate(main = map(text, str_split, pattern = "\n\n")) -> elo #%>%
+           #map(~.x[[1]][[]] ) ) -> elo
 
+elo$main[[1]] %>% unlist() -> one_article
+one_article %>% length() -> length_paragraph
+
+
+
+
+         main = map(text, str_split, pattern = "\n\n") %>%
+           map(~.x[[1]])) -> elo
+elo %>%
+  mutate(header = as.character(header) )
+
+  
+  DATA %>%
+    mutate(text = map_chr(text, str_remove_all, pattern = num_pattern ) ) %>%
+    separate(text, "\n\n")
+
+
+
+  mutate(header = toString(header))-> elo
+#-----------
+elo$header_help[[1]] %>% map(~.x[[1]])
+#----------
+  unnest_tokens(word, text) %>%
+  anti_join(get_stopwords()) %>%
+  nest(-name, -value, -category, -path, -in_train) %>%
+  
+
+
+
+#------------
 maxlenth <- 100
 maxwords <- 1e4
 
@@ -52,7 +92,8 @@ sesja$run()
 tokenize_text <- function(zbior, 
                           max_len = 100,
                           max_words = 10000){
-  tokenizer <- text_tokenizer(num_words = max_words) %>%
+  tokenizer <- text_tokenizer(num_words = max_words,
+                              filters = "!\"#%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n") %>%
     fit_text_tokenizer(zbior$text)
   
   sekwencja <- texts_to_sequences(tokenizer, zbior$text )
@@ -62,7 +103,7 @@ tokenize_text <- function(zbior,
   return(data)
 }
 
-
+sample_data %>% tokenize_text()
 
 
 
